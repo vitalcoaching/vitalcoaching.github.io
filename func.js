45a6c8a41e60b6d6a98d4e29380b4328467a291e
vitalcoaching <support@stylifyyourblog.com>
2016-04-27 04:10:04 +0530

    Update func.js

--- func.js
+++ func.js
@@ -138,3 +138,55 @@ function mythumb66(json) {
             eval(document.write.END);
           }
 
+          function mythumb88(json) {
+            eval(document.write.START);
+            document.write('<h2><a href="'+ json.feed.link[2].href +'" class="xpose_meta_label_names">' + json.feed.link[2].href.replace(/%20/g," ").split("/search/label/")[1] + '</a></h2>');
+            for (var i = 0; i < json.feed.entry.length; i++) {
+              var entry = json.feed.entry[i];
+              var posttitle = entry.title.$t;
+              var posturl;
+              if (i == json.feed.entry.length) break;
+              for (var k = 1; k < entry.link.length; k++) {
+                if (entry.link[k].rel == 'replies' && entry.link[k].type == 'text/html') {
+                  var commenttext = entry.link[k].title;
+                  var commenturl = entry.link[k].href;
+                }
+                if (entry.link[k].rel == 'alternate') {
+                  posturl = entry.link[k].href;
+                  break;
+                }
+              }
+              var thumburl2;    
+              try {
+                thumburl2 = entry.media$thumbnail.url.replace("/s72-c/","/w"+thumb_width2+"-h"+thumb_height2+"-c/");
+              } catch (error) {
+                s = entry.content.$t;
+                a = s.indexOf("<img");
+                b = s.indexOf("src=\"", a);
+                c = s.indexOf("\"", b + 5);
+                d = s.substr(b + 5, c - b - 5);
+                if(json.feed.link[2].href.replace(/%20/g," ").split("/search/label/")[1] == "dating for women"){noThumbArrays = dfw;}else 
+                if(json.feed.link[2].href.replace(/%20/g," ").split("/search/label/")[1] == "I have a crush"){noThumbArrays = ihac;}else 
+                if(json.feed.link[2].href.replace(/%20/g," ").split("/search/label/")[1] == "dating for men" || json.feed.link[2].href.replace(/%20/g," ").split("/search/label/")[1] == "dating for men articles" || json.feed.link[2].href.replace(/%20/g," ").split("/search/label/")[1] == "dating for men audios" || json.feed.link[2].href.replace(/%20/g," ").split("/search/label/")[1] == "dating for men videos"){noThumbArrays = dfm;} 
+                else{noThumbArrays = noThumbArray;}
+                if ((a != -1) && (b != -1) && (c != -1) && (d != "")) {
+                  thumburl2 = d;
+                } else thumburl2 = noThumbArrays[Math.ceil(Math.random() * (noThumbArrays.length - 0)) - 1];
+              } 
+              var postdate = entry.published.$t;
+              var cdyear = postdate.substring(0, 4);
+              var cdmonth = postdate.substring(5, 7);
+              var cdday = postdate.substring(8, 10);
+              var towrite = '';
+                document.write('<a href="' + posturl + '"><span class="xpose_thumb2"><img width="' + thumb_width2 + '" height="' + thumb_height2 + '" alt="' + posttitle + '" src="' + thumburl2.replace("/default.jpg","/hqdefault.jpg") + '"/></span></a>'+ '<span class="linkshortcode"><a href="'+ posturl +'" class="xpose_meta_label_names">' + posttitle + '</a></span>');
+              if(json.feed.entry.length == json.feed.openSearch$itemsPerPage.$t && i == (json.feed.entry.length-1)) {
+               document.write('<a href="'+ json.feed.link[2].href +'" class="read_more">></a>');
+              }
+              if (showcommentnum2 == true) {
+                if (commenttext == '1 Comments') commenttext = '1 Comment';
+                if (commenttext == '0 Comments') commenttext = 'No Comments';
+              }
+             
+            }
+            eval(document.write.END);
+          }
