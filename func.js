d8219faae1850c6c7a589483e25d400b7b267e74
vitalcoaching <support@stylifyyourblog.com>
2016-04-29 04:52:07 +0530

    Update func.js

--- func.js
+++ func.js
@@ -391,7 +391,7 @@ function mythumb66(json) {
               var towrite = '';
                 document.write('<a href="' + posturl + '"><span class="xpose_thumb2"><img width="' + thumb_width2 + '" height="' + thumb_height2 + '" alt="' + posttitle + '" src="' + thumburl2.replace("/default.jpg","/hqdefault.jpg") + '"/></span></a>'+ '<span class="linkshortcode"><a href="'+ posturl +'" class="xpose_meta_label_names">' + posttitle + '</a></span>');
               if(json.feed.entry.length == json.feed.openSearch$itemsPerPage.$t && i == (json.feed.entry.length-1)) {
-               document.write('<a href="'+ json.feed.link[2].href +'" class="read_more">></a>');
+               document.write('<span class="more_read_link"><a href="'+ json.feed.link[2].href +'" class="read_more">></a></span>');
               }
               if (showcommentnum2 == true) {
                 if (commenttext == '1 Comments') commenttext = '1 Comment';
@@ -449,7 +449,7 @@ function mythumb66(json) {
               var towrite = '';
                 document.write('<a href="' + posturl + '"><span class="xpose_thumb2"><img width="' + thumb_width2 + '" height="' + thumb_height2 + '" alt="' + posttitle + '" src="' + thumburl2.replace("/default.jpg","/hqdefault.jpg") + '"/></span></a>'+ '<span class="linkshortcode"><a href="'+ posturl +'" class="xpose_meta_label_names">' + posttitle + '</a></span>');
               if(json.feed.entry.length == json.feed.openSearch$itemsPerPage.$t && i == (json.feed.entry.length-1)) {
-               document.write('<a href="'+ json.feed.link[2].href +'" class="read_more">></a>');
+               document.write('<span class="more_read_link"><a href="'+ json.feed.link[2].href +'" class="read_more">></a></span>');
               }
               if (showcommentnum2 == true) {
                 if (commenttext == '1 Comments') commenttext = '1 Comment';
